b763144227c59b0d0d88e1326ac624ad8f607c1d
ToLa09 <landgraf-tobias@web.de>
2022-11-09 11:10:18 +0100

    backend: rename application.java

--- backend/src/main/java/de/javaffm221firstMongoDB/BackendApplication.java
+++ backend/src/main/java/de/javaffm221firstMongoDB/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaFfm221FirstDockerApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaFfm221FirstDockerApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
